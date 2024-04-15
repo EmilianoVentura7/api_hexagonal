6b0c26cd5a20f51688fde89a4de60eab861d37c7
EmilianoVentura7 <223314@ids.upchiapas.edu.mx>
2024-04-15 12:15:54 -0600

    db host

--- src/database/mysql.ts
+++ src/database/mysql.ts
@@ -6,6 +6,7 @@ dotenv.config();
 const signale = new Signale();
 
 const config = {
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
